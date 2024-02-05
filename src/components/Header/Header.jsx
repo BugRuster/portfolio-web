90e8e6188bcebff1c613b19af962db1a52c2239a
Ashish Kumar <akdev5520@gmail.com>
2024-02-05 19:26:28 +0530

    added nav component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,9 @@ function Header() {
 			<div className="container header-container">
 				<h5>Hello I'm</h5>
 				<h1>Anurag Sharma</h1>
-				<h5 className="text-light">Frontend Developer</h5>
+				<h5 className="text-light">
+					Flutter Developer | Cyber Security | CodeChef
+				</h5>
 				{/* call to action */}
 				<CTA />
 				<HeaderSocials />
